c72a1af1f44bc7cb601c701a076f69e8aad87e8d
Samuel Spink <dr.greenjet@gmail.com>
2025-01-29 21:25:21 +0100

    feat: package module exports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+const { loadConfig } = require("./utils/configUtils");
+const Account = require("./models/account");
+const Transaction = require("./models/transaction");
+const Book = require("./models/book");
+const Engine = require("./models/engine");
 
-// TODO
+module.exports = {
+  loadConfig,
+  Account,
+  Transaction,
+  Book,
+  Engine
+};

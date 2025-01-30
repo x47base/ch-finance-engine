74457054352d7e7fec30ffab3c1cd8bf6d65e31a
Samuel Spink <dr.greenjet@gmail.com>
2025-01-30 10:04:38 +0100

    feat: loadConfig function

--- src/utils/configUtils.js
+++ src/utils/configUtils.js
@@ -1,5 +1,25 @@
-const loadConfig = (configPath) => {
-    // TODO
-};
+const fs = require("fs");
+const path = require("path");
+
+/**
+ * Loads and parses a JSON config file.
+ * @param {string} configPath
+ * @returns {object}
+ */
+function loadConfig(configPath) {
+    let absolutePath;
+    if (configPath === "../config/standardConfig.json") {
+        absolutePath = path.resolve(
+        __dirname,
+        "..",
+        "config",
+        "standardConfig.json"
+        );
+    } else {
+        absolutePath = path.resolve(__dirname, configPath);
+    }
+    const rawData = fs.readFileSync(absolutePath, "utf-8");
+    return JSON.parse(rawData);
+}
 
 module.exports = { loadConfig };

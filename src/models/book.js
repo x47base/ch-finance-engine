2f807b0010bbbcdae7a352aee8a9d048a02d8bc4
Samuel Spink <dr.greenjet@gmail.com>
2025-01-30 10:04:05 +0100

    feat: book class fully completed

--- src/models/book.js
+++ src/models/book.js
@@ -1,27 +1,65 @@
-
+/**
+ * Represents an accounting year.
+ */
 class Book {
     /**
-     * @param {number} year - The year for which the accounting book is.
+     * @param {number} year - Accounting year.
      */
     constructor(year) {
-        /**
-         * The account's accounting book year.
-         * @type {number}
-         */
+        if (!Number.isInteger(year) || year < 0) {
+            throw new Error("Invalid year for Book.");
+        }
         this._year = year;
-
-        // TODO
+        this._accounts = [];
+        this._closed = false;
     }
 
     /**
-     * Returns the account's accounting year (read-only).
      * @returns {number}
      */
     get year() {
         return this._year;
     }
 
-    // TODO
+    /**
+     * @returns {boolean}
+     */
+    get isClosed() {
+        return this._closed;
+    }
+
+    closeBook() {
+        this._closed = true;
+        for (const acc of this._accounts) {
+            acc.markFullyBookedForYear();
+        }
+    }
+
+    reopenBook() {
+        this._closed = false;
+    }
+
+    /**
+     * @param {Account} account
+     */
+    addAccount(account) {
+        this._accounts.push(account);
+    }
+
+    /**
+     * @returns {Array<Account>}
+     */
+    get accounts() {
+        return this._accounts;
+    }
+
+    toJSON() {
+        return {
+            year: this._year,
+            closed: this._closed,
+            accounts: this._accounts.map(acc => acc.toJSON())
+        };
+    }
 }
 
 module.exports = Book;
